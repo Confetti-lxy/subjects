9faabfd1e639fd66bf0f20397ef5b9bbe0fb2d5d
Confetti-lxy <3038454387@qq.com>
2022-07-04 12:23:01 +0800

    Enter

--- 实验.cpp
+++ 实验.cpp
@@ -1,19 +1,60 @@
-#include <iostream>
-#include <climits>
-using namespace std;
+#include <stdio.h>
 
-struct node
+void DigiTran(int val, char res[]);
+
+int main()
 {
-    int to, length, index;
-    node() : to(0), length(0), index(0) {}
-    node(int to, int length, int index) : to(to), length(length), index(index) {}
-};
+    int val = 0;
+    while ((scanf("%d", &val)) != -1)
+    {
+        char res[10004];
+        DigiTran(val, res);
+    }
+}
 
-struct point
+void DigiTran(int val, char res[])
 {
-    long dis, index;
-    point() : dis(0), index(0) {}
-    point(int dis, int index) : dis(dis), index(index) {}
-};
-
-int dijkstra(int start, int end);
+    int index = 0, flag = 0, change_value = 0;
+    if (val % 10 == 0)
+    {
+        flag = 1;
+    }
+    while (val != 0)
+    {
+        int temp = val % 10;
+        change_value = change_value * 10 + temp;
+        val /= 10;
+    }
+    while (change_value >= 10)
+    {
+        int temp1 = change_value % 10, temp2 = (change_value % 100) / 10;
+        int key = temp1 * 10 + temp2;
+        if (key < 26)
+        {
+            res[index] = 'A' + key;
+            index++;
+        }
+        else
+        {
+            res[index] = 'A' + temp1;
+            index++;
+            res[index] = 'A' + temp2;
+            index++;
+        }
+        change_value /= 100;
+    }
+    if (change_value != 0)
+    {
+        res[index] = 'A' + change_value;
+    }
+    for (int i = 0; i < index; i++)
+    {
+        printf("%c", res[i]);
+    }
+    if (flag == 1)
+    {
+        printf("A");
+    }
+    printf(" ");
+    return;
+}
