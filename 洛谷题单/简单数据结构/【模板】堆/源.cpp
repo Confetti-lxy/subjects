836fafa6009ef5a941190378578816d2d4ee229b
Confetti-lxy <3038454387@qq.com>
2022-07-05 14:21:21 +0800

    Enter

---
+++ 洛谷题单/简单数据结构/【模板】堆/源.cpp
@@ -0,0 +1,30 @@
+#include <vector>
+#include <queue>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    priority_queue<int, vector<int>, greater<int>> que;
+    int n = 0, x = 0, y = 0;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> x;
+        if (x == 1)
+        {
+            cin >> y;
+            que.push(y);
+        }
+        else if (x == 2)
+        {
+            cout << que.top() << endl;
+        }
+        else
+        {
+            que.pop();
+        }
+    }
+    return 0;
+}
