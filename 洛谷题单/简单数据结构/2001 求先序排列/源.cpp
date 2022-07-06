0db67179c6c82a2d0104cfe2f50b7dd81d994afa
Confetti-lxy <3038454387@qq.com>
2022-07-06 23:57:11 +0800

    Enter

---
+++ 洛谷题单/简单数据结构/2001 求先序排列/源.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct TreeNode
+{
+    char word;
+    TreeNode *left, *right;
+
+    TreeNode(char word) : word(word), left(NULL), right(NULL) {}
+
+    TreeNode(char word, TreeNode *left, TreeNode *right) : word(word), left(left), right(right) {}
+};
+
+TreeNode *createTree(string str1, string str2);
+
+void trace(TreeNode *root);
+
+string ans = "";
+
+int main()
+{
+    string str1, str2;
+    cin >> str1 >> str2;
+    TreeNode *root = createTree(str1, str2);
+    trace(root);
+    cout << ans << endl;
+}
+
+TreeNode *createTree(string str1, string str2)
+{
+    if (str1.length() == 0)
+    {
+        //        cout << -1 << endl;
+        return NULL;
+    }
+    char start = str2[str2.length() - 1];
+    TreeNode *node = new TreeNode(start);
+    if (str1.length() == 1)
+    {
+        //        cout << 0 << endl;
+        return node;
+    }
+    else
+    {
+        int loc = -1;
+        for (int i = 0; i < str1.length(); i++)
+        {
+            if (str1[i] == start)
+            {
+                loc = i;
+                break;
+            }
+        }
+        string str3 = str1.substr(0, loc);
+        string str4 = str1.substr(loc + 1, str1.length() - 1);
+        string str5 = str2.substr(0, loc);
+        string str6 = str2.substr(loc, str2.length() - 1 - loc);
+        if (loc == str2.length() - 1)
+        {
+            str6 = "";
+        }
+        //        cout << loc << " " << str3 << " " << str4 << " " << str5 << " " << str6 << endl;
+        node->left = createTree(str3, str5);
+        node->right = createTree(str4, str6);
+        return node;
+    }
+}
+
+void trace(TreeNode *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    ans += root->word;
+    trace(root->left);
+    trace(root->right);
+}
---
+++ 洛谷题单/简单数据结构/2004 FBI 树/源.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct TreeNode
+{
+    char str;
+    TreeNode *left, *right;
+
+    TreeNode(char str) : str(str), left(NULL), right(NULL) {}
+
+    TreeNode(char str, TreeNode *left, TreeNode *right) : str(str), left(left), right(right) {}
+};
+
+char judge(string str);
+
+TreeNode *create(string my_str);
+
+void trace(TreeNode *root);
+
+string ans = "";
+
+int main()
+{
+    int n = 0;
+    string str;
+    cin >> n >> str;
+    TreeNode *root = create(str);
+    trace(root);
+    cout << ans << endl;
+}
+
+char judge(string str)
+{
+    int t1 = 0, t2 = 0;
+    for (char word : str)
+    {
+        if (word == '1')
+        {
+            t1++;
+        }
+        if (word == '0')
+        {
+            t2++;
+        }
+    }
+    if (t1 == 0)
+    {
+        return 'B';
+    }
+    else if (t2 == 0)
+    {
+        return 'I';
+    }
+    else
+    {
+        return 'F';
+    }
+}
+
+TreeNode *create(string my_str)
+{
+    char s = judge(my_str);
+    TreeNode *node = new TreeNode(s);
+    if (my_str.length() == 1)
+    {
+        return node;
+    }
+    else
+    {
+        int size = my_str.length() / 2;
+        string str1 = my_str.substr(0, size);
+        string str2 = my_str.substr(size, my_str.length() - 1);
+        node->left = create(str1);
+        node->right = create(str2);
+        return node;
+    }
+}
+
+void trace(TreeNode *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    trace(root->left);
+    trace(root->right);
+    ans += root->str;
+}
