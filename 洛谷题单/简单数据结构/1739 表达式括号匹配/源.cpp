4b85dfce60d12ff8df1870e8ba390921f0ff2af0
Confetti-lxy <3038454387@qq.com>
2022-07-11 12:19:39 +0800

    update

---
+++ 洛谷题单/简单数据结构/1739 表达式括号匹配/源.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    stack<char> my_stack;
+    char word;
+    cin >> word;
+    while (word != '@')
+    {
+        if (word == '(')
+        {
+            my_stack.push(word);
+        }
+        else if (word == ')')
+        {
+            if (!my_stack.empty())
+            {
+                if (my_stack.top() == '(')
+                {
+                    my_stack.pop();
+                }
+                else
+                {
+                    my_stack.push(word);
+                }
+            }
+            else
+            {
+                my_stack.push(word);
+            }
+        }
+        cin >> word;
+    }
+    if (my_stack.empty())
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    };
+}
