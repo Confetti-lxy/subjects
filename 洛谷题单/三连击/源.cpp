3111039063ff0587277916613b29535c6760623a
Confetti-lxy <3038454387@qq.com>
2022-07-29 17:21:03 +0800

    Enter

--- 洛谷题单/三连击/源.cpp
+++ 洛谷题单/三连击/源.cpp
@@ -1,41 +1,34 @@
 #include <iostream>
 #include <vector>
+
 using namespace std;
 
-int main()
-{
+int main() {
     vector<int> nums(10, 0);
     int flag = 1;
-    for (int i = 123; i < 333; i++)
-    {
+    for (int i = 123; i < 333; i++) {
         nums.clear();
         nums.resize(10, 0);
         flag = 1;
         int x = i, y = i * 2, z = i * 3;
-        while (x != 0)
-        {
+        while (x != 0) {
             nums[x % 10]++;
             x /= 10;
         }
-        while (y != 0)
-        {
+        while (y != 0) {
             nums[y % 10]++;
             y /= 10;
         }
-        while (z != 0)
-        {
+        while (z != 0) {
             nums[z % 10]++;
             z /= 10;
         }
-        for (int j = 1; j < 10; j++)
-        {
-            if (nums[j] == 0)
-            {
+        for (int j = 1; j < 10; j++) {
+            if (nums[j] == 0) {
                 flag = 0;
             }
         }
-        if (flag == 1)
-        {
+        if (flag == 1) {
             cout << i << " " << i * 2 << " " << i * 3 << endl;
         }
     }
